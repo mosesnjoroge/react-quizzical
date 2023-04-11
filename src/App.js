c57674b5797c70c0d59e7dc2e0b2330f1d613a3f
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-11 10:52:47 -0400

    quiz component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div className="App">
-
+      <Quiz/>
     </div>
   );
 }
---
+++ src/components/Quiz.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function Quiz(){
+
+
+  return (
+    <div>
+      <div>
+        <h3>How would one say goodbye in Spanish?</h3>
+        <button>Adios</button>
+        <button>Hola</button>
+        <button>Au Revior</button>
+        <button>Salir</button>
+      </div>
+
+      <div>
+        <h3>Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h3>
+        <button>Cabbage Patch Kids</button>
+        <button>Transformers</button>
+        <button>Care bears</button>
+        <button>Rubik's Cube</button>
+      </div>
+
+      <div>
+        <h3>What is the hottest plante in our Solar System?</h3>
+        <button>Mercury</button>
+        <button>Venus</button>
+        <button>Mars</button>
+        <button>Saturn</button>
+      </div>
+
+      <div>
+        <h3>In which country was the ceasar salad invented?</h3>
+        <button>Italy</button>
+        <button>Portugal</button>
+        <button>Mexico</button>
+        <button>France</button>
+      </div>
+
+      <div>
+        <h3>How many hearts does An Octopus have?</h3>
+        <button>One</button>
+        <button>Two</button>
+        <button>Three</button>
+        <button>Four</button>
+      </div>
+
+    </div>
+  );
+}
