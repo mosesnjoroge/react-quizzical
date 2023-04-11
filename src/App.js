ea140b17e30d3cbfb54588131f091d9141df43a1
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-11 12:16:41 -0400

    created initial states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
+  // states
+  const[questions,setQuestions] = useState([])
+  const[started, setStarted] = useState(false)
+  const[correct,setCorrect] = useState(0)
+  const[checked, setChecked] = useState(false)
+  const[count, setCount] = useState(0)
+
+  //  // componentDidMount() method to fetch questions from the API
+  // componentDidMount = ()=>{
+
+  //   // Fetching data from the API
+  //   fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  //     // Converting the promise received into JSON
+  //     .then(response => response.json())
+  //     .then(content =>
+  //         // Updating state variables
+  //       this.setState({
+  //         allMemeImgs: content.data.memes
+  //       })
+  //     );
+  // }
+  // // loop for printing out questions
 
 
+  // // Method to submit form and create meme
+  // handleSubmit = event =>{
+  //   event.preventDefault();
+  //   const { allMemeImgs } = this.state;
+  //   const rand = allMemeImgs[
+  //     Math.floor(Math.random() * allMemeImgs.length)].url;
+  //   this.setState({
+  //     randomImg: rand
+  //   });
+  // };
 
   return (
     <div className="App">
