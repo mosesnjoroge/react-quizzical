3024d0311c6c264e87dba6dd75e4c195aa354df0
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-15 15:47:47 -0400

    first commit

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,14 +1,29 @@
+import { nanoid } from "nanoid";
 import React from "react";
 // import nanoid from 'nanoid';
 
 export default function Quiz(props){
- let answers = props.q.answers
 
+  let answers = props.q.answers
+  const answerElement = answers.map(answer => {
+    let id = null;
+    if (props.q.checked){
+      if (props.q.correct === answer){
+        id = 'correct'
+      }
+      else if (props.q.selected === answer){
+        id = 'incorrect'
+      }
+      else {
+        id = 'not-selected'
+      }
+    }
+  })
   return (
     <div>
       <div>
         <h3>{props.q}</h3>
-        <button>{answers}</button>
+        <button key = {nanoid()} id = {id} className="">{answerElement}</button>
       </div>
       <div>
         <h3>You scored {props.correct} correct answers</h3>
