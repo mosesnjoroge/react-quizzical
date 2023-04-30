97bf7a16c4616e08397c06989391824a7616fcf6
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-30 14:55:27 -0400

    file cleanup

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,37 +6,39 @@ export default function Quiz(props){
 
   let answers = props.q.answers
 
+  // answer validation
   function handleClick(answer) {
     if (props.q.checked){
       return
     }
     props.handleClickAnswer(props.id, answer)
   }
+  // rendering answer elements
+    const answerElements = answers.map(answer => {
+      let id = null;
+      if (props.q.checked){
+        if (props.q.correct === answer){
+          id = 'correct'
+        }
+        else if (props.q.selected === answer){
+          id = 'incorrect'
+        }
+        else {
+          id = 'not-selected'
+        }
+      }
 
-  const answerElements = answers.map(answer => {
-    let id = null;
-    if (props.q.checked){
-      if (props.q.correct === answer){
-        id = 'correct'
-      }
-      else if (props.q.selected === answer){
-        id = 'incorrect'
-      }
-      else {
-        id = 'not-selected'
-      }
-    }
-    return (
-      <button
-        key={nanoid()}
-        id={id}
-        className={answer === props.q.selected? "answer selected" : "answer"}
-        onClick={() => handleClick(answer)}
-        >
-          {answer}
-      </button>
-    )
-  })
+      return (
+        <button
+          key={nanoid()}
+          id={id}
+          className={answer === props.q.selected? "answer selected" : "answer"}
+          onClick={() => handleClick(answer)}
+          >
+            {answer}
+        </button>
+      )
+    })
 
   return (
     <div className="question-container">
