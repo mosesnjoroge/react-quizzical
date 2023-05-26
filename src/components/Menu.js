02b5f58ca12c36c9b50fc28201e06e7cccda4ba9
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-05-26 18:52:07 -0400

    general package cleanup and removal of fontawesome pro icon import

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-// import { CIcon } from '@coreui/icons-react';
-// import { cilPencil, cilPenAlt } from '@coreui/icons';
+
 
 export default function Menu(props) {
 
   return(
 
       <div className='homepage'>
-        {/* <div>
-          <CIcon icon={cilPencil} size = "l"/>
-          <CIcon icon={cilPenAlt } size = "l"/>
-        </div> */}
+
         <h2 className='page-title'>Welcome to Quizzical</h2>
         <div>
           <Button variant = 'outline-primary' onClick={() => props.start()}>Start</Button>
