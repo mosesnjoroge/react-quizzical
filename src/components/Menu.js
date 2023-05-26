6e7c5e589c04f7d1f92ff235f897d8e53044c6f8
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-05-26 19:15:51 -0400

    codr formatting on menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,14 +7,14 @@ export default function Menu(props) {
 
   return(
 
-      <div className='homepage'>
-        <div className='homepage-icons'>
-          <FontAwesomeIcon icon={faPenToSquare} bounce size = "lg" style={{color: "#594dda",}} />
-        </div>
-        <h2 className='page-title'>Welcome to Quizzical</h2>
-        <div>
-          <Button variant = 'outline-primary' onClick={() => props.start()}>Start</Button>
-        </div>
+    <div className='homepage'>
+      <div className='homepage-icons'>
+        <FontAwesomeIcon icon={faPenToSquare} bounce size = "lg" style={{color: "#594dda",}} />
       </div>
+      <h2 className='page-title'>Welcome to Quizzical</h2>
+      <div>
+        <Button variant = 'outline-primary' onClick={() => props.start()}>Start</Button>
+      </div>
+    </div>
   )
 }
