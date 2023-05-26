fe12c320f2bbc9bae33390f606ada1d71e5bc38e
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-05-26 18:32:06 -0400

    removed babel macros package from fontawesome

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { CIcon } from '@coreui/icons-react';
+import { cilPencil } from '@coreui/icons';
 
 export default function Menu(props) {
 
   return(
       <div className='homepage'>
+        <CIcon icon={cilPencil} style={{color: "#5b4ce2;",}} />
         <h2 className='page-title'>Welcome to Quizzical</h2>
         <div>
           <Button variant = 'outline-primary' onClick={() => props.start()}>Start</Button>
