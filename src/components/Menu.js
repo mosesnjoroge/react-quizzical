1afbcb7137af7c3bb362965a2a08671b2f050073
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-05-26 19:46:12 -0400

    refactoring app css stylesheet and dynamically importing component css rules/ positioning animated icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,11 +5,12 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Menu(props) {
 
+
   return(
 
     <div className='homepage'>
-      <div className='homepage-icons'>
-        <FontAwesomeIcon icon={faPenToSquare} bounce size = "lg" style={{color: "#594dda",}} />
+      <div className='homepage-icon d-flex'>
+        <FontAwesomeIcon icon={faPenToSquare} bounce size = "xl" style={{color: "#594dda",}} />
       </div>
       <h2 className='page-title'>Welcome to Quizzical</h2>
       <div>
