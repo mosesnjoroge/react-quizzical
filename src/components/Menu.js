d32c3fdcd9f5b1b4b6e4a6df579cb505a200c2dc
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-05-26 20:04:24 -0400

    removed icons8 as blob supplier

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -17,7 +17,7 @@ export default function Menu(props) {
         <Button variant = 'outline-primary' onClick={() => props.start()}>Start</Button>
       </div>
       <div>
-        <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
+
       </div>
     </div>
   )
